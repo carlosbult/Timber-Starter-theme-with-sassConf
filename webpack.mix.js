d757366610d3a0aaaa45d5a48f355749c2cc4bb7
carlos bult <acevedo.bult@gmail.com>
2022-05-03 16:40:27 -0430

    First Commit

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+const mix = require('laravel-mix');
+
+mix
+  .js('assets/js/app.js', 'js')
+  .sass('assets/sass/style.scss', 'css')
+  .setPublicPath('dist')
+  .options({
+    autoprefixer: { remove: false },
+  });
